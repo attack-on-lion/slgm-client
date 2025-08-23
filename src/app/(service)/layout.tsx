5f787e03ea9aa300f0609cb7e08b7ac88ae753cf
minhyeong <minhyeong.dev@gmail.com>
2025-08-23 09:27:46 +0900

    fix: manage overflow control

--- src/app/(service)/layout.tsx
+++ src/app/(service)/layout.tsx
@@ -4,9 +4,11 @@ import ServiceNavigator from "@/components/molecule/(service)/Navigator"
 
 export default function ServiceLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative h-screen bg-bg-color pb-[49px] overflow-hidden">
+    <div className="relative overflow-hidden">
+    <div className="h-screen bg-bg-color pb-[49px] overflow-y-scroll">
       {children}
       <ServiceNavigator />
+    </div>
     </div>
   )
 }
