3c0385f58754387762a0f3795d8d45fc4103b32a
minhyeong <minhyeong.dev@gmail.com>
2025-08-24 23:41:04 +0900

    feat: create interfaces

--- src/interfaces/Store.ts
+++ src/interfaces/Store.ts
@@ -1,2 +1,10 @@
 export const storeTypes=['전체','음식점','카페','편의점','마트','기타'] as const;
 export type StoreType=typeof storeTypes[number];
+
+export interface Item{
+	storeName:string,
+	name:string,
+	point:number,
+	imageUrl:string,
+	storeType:StoreType,
+}
