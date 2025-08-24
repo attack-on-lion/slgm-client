606dcc0d89a32ed36652b91294cb7284b7a6ef82
minhyeong <minhyeong.dev@gmail.com>
2025-08-24 23:40:17 +0900

    feat: create right node

--- src/components/atom/Section.tsx
+++ src/components/atom/Section.tsx
@@ -3,13 +3,14 @@ import { cn } from "fast-jsx/util";
 interface Props{
 	title?:string,
 	children:React.ReactNode,
+	right?:React.ReactNode,
 	option?:{
 		smallTitle?:boolean,
 		noPadding?:boolean,
 	}
 }
 
-export default function Section({title,children,option}:Props){
+export default function Section({title,children,right,option}:Props){
 	const container={
 		display:'flex flex-col gap-y-[16px]',
 		background:'bg-white',
@@ -17,6 +18,7 @@ export default function Section({title,children,option}:Props){
 		padding:'pt-[24px]',
 	}
 	const titleBox={
+		display:'flex flex-row justify-between items-center',
 		styles:'leading-none font-bold',
 		titleSize:option?.smallTitle?'text-[16px]':'text-[20px]',
 		padding:'px-[24px]'
@@ -25,7 +27,9 @@ export default function Section({title,children,option}:Props){
 		padding: !option?.noPadding&&'p-[24px]',
 	}
 	return <div className={cn(container)}>
-		{title && <div className={cn(titleBox)}>{title}</div>}
+		{title && <div className={cn(titleBox)}>{title}
+			{right && <div >{right}</div>}
+			</div>}
 		<div className={cn(body)}>{children}</div>
 	</div>
 } 
